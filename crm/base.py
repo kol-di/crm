a7bc7a9f3068bc00f433651fc298436ece4e9901
kol-di <60890747+kol-di@users.noreply.github.com>
2021-07-27 19:36:32 +0300

    now permanent database is created, which is not deleted after program execution

--- crm/base.py
+++ crm/base.py
@@ -3,12 +3,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # create an engine
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+engine = create_engine("sqlite:///:db", echo=True, future=True)
 
-# create "Session" class
+# create base class
+Base = declarative_base()
+
+# Create all the tables in the database which are
+# defined by Base's subclasses
+Base.metadata.create_all(engine)
+
+# Construct a sessionmaker factory object
+# and bind the sessionmaker to engine
 Session = sessionmaker(bind=engine)
 
 # create a new session
 session = Session()
-
-Base = declarative_base()
